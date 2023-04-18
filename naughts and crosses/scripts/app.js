b9903c4bf68a28a1c1907dda9d78b1408e72490a
A-Waterhouse <123133098+A-Waterhouse@users.noreply.github.com>
2023-04-18 03:22:15 +0100

    18/4/23

--- naughts and crosses/scripts/app.js
+++ naughts and crosses/scripts/app.js
@@ -1,5 +1,20 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "X"
+  },
+  {
+    name: "",
+    symbol: "O"
+  },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn"); // gets hold of the buttons
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -8,5 +23,6 @@ const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig); //listens for click on mouse, then executes a function in config.js but config must be loaded before this file
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
-backdropElement.addEventListener("click", closePlayerConfig);//can run function on click of background
+backdropElement.addEventListener("click", closePlayerConfig); //can run function on click of background
 
+formElement.addEventListener("submit", savePlayerConfig);
