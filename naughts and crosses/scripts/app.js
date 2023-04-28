9d0e99c3f843197c4c41abb9610b573d1cb958a6
A-Waterhouse <123133098+A-Waterhouse@users.noreply.github.com>
2023-04-28 03:13:30 +0100

    28/4/23

--- naughts and crosses/scripts/app.js
+++ naughts and crosses/scripts/app.js
@@ -1,4 +1,11 @@
+const gameData = [
+[0, 0, 0],
+[0, 0, 0],
+[0, 0, 0],
+]
+
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -15,10 +22,14 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn"); // gets hold of the buttons
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameFieldElements = document.querySelectorAll("#game-board li"); //selects all list items in the game-board ID
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig); //listens for click on mouse, then executes a function in config.js but config must be loaded before this file
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -26,3 +37,9 @@ cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig); //can run function on click of background
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener("click", selectGameField);
+}
